5043a5905e23b0f330d08e3f0d40fe00eea2dbbe
IlyassBacime <ibacime@gmail.com>
2023-10-05 19:38:33 -0700

    Helloooo

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,10 @@
 #include <unistd.h>
 /**
  * main - This program will run a poem using the fput function
- * Description: Main indicates 1
  * Return 1 (Success)
  */
 int main(void)
 {
-fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 9);
 return (1);
 }
