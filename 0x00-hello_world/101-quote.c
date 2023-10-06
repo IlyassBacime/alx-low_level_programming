05c1173c66d79e2bb5b5d658cd8e0ff1729f33bd
IlyassBacime <ibacime@gmail.com>
2023-10-05 20:03:23 -0700

    Modified answer

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,11 @@
 #include <unistd.h>
-#include <stdio.h>
 /**
- * main - This program will run a poem using the fput function
- * Return 1 (Success)
+ * main - prints and that piece of art 
+ * Return: Always 1
  */
+
 int main(void)
 {
-fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 return (1);
 }
