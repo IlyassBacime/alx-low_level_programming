9b24b02d84a67c4071dfae8ddf757a2514824633
IlyassBacime <ibacime@gmail.com>
2023-10-05 18:21:29 -0700

    Programming C with printf function to get data size with %lu

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- * main - This programi runs the size of data using the printf function
+ * main - This program runs the size of data using the printf function
  * Return: 0 (Success)
  */
 int main(void)
@@ -9,12 +9,12 @@ int main(void)
 	int b;
 	long int c;
 	long long int d;
-	float e;
+	float f;
 
 printf("Size of a char: %lu bytes(n)\n", (unsigned long)sizeof(a));
 printf("Size of a int: %lu bytes(n)\n", (unsigned long)sizeof(b));
 printf("Size of a long int: %lu bytes(n)\n", (unsigned long)sizeof(c));
 printf("Size of a long long int: %lu bytes(n)\n", (unsigned long)sizeof(d));
-printf("Size of a float: %lu bytes(n)\n", (unsigned long)sizeof(e));
+printf("Size of a float: %lu bytes(n)\n", (unsigned long)sizeof(f));
 return (0);
 }
