685f388dad6591775a329ab18772f441f830005b
IlyassBacime <ibacime@gmail.com>
2023-10-06 14:56:26 -0700

    Prints digit of base 10 without char

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Prints digits of base 10 starting from 0 without char
+ *
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	int i;
+
+for (i = 8; i < 18; i++)
+{
+	putchar(i);
+}
+putchar('\n');
+return (0);
+}
