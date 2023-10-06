2a4b40d80b14ec57b0b7a5da48d3f0d0184c6091
IlyassBacime <ibacime@gmail.com>
2023-10-06 14:28:26 -0700

    Prints the alphabet besides q and e char

---
+++ 0x01-variables_if_else_while/4-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Prints alphabet without the q and e
+ *
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	char alp[24] = "abcdfjklmnoprstuvwxyz";
+	int i;
+
+for (i = 0; i < 24; i++)
+{
+	putchar(alph[i]);
+}
+putchar('\n');
+return (0);
+}
