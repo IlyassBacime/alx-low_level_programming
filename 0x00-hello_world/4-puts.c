b5eb1bdde7b22d29516d3cd3e89ed75023ace005
IlyassBacime <ibacime@gmail.com>
2023-10-05 17:35:41 -0700

    Programming C with puts function

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - This programm is ran with puts function
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
