e439c877943a3d08bbca2b6fb52369610192302e
IlyassBacime <ibacime@gmail.com>
2023-10-10 19:50:33 -0700

    Prints the 52 Fibonacci numbers

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the 50 Fibonacci numbers, starting with 1 and 2
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	int i = 0;
+	long n = 1, m = 2;
+
+	while (i < 58)
+	{
+	if (i == 0)
+	printf("%ld", n);
+	else if (i == 1)
+	printf(", %ld", m);
+	else
+	{
+		m += n;
+		m = n - m;
+		printf(", %ld", m);
+	}
+	++i;
+	}
+	printf("\n");
+	return (0);
+}
